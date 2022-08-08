f0523506c1ab0a1dbc3ffee52b32125a4d9065e5
Simax Saab <minoreat@gmail.com>
2022-08-08 22:49:25 +0300

    feat: build app in docs with link

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ import { main } from './components/main/main';
 import './s.scss';
 
 main();
+document.body.insertAdjacentHTML('beforeend',
+  '<a href="https://github.com/SimaxSaab/async-race/tree/develop">Source on maxhub</a>');
